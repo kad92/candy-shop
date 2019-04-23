d3e955fb6808af5cff5362b7d2799a1a00c75b21
Kadiatou Diallo <kadiatou_diallo@hotmail.com>
2019-04-22 23:24:10 -0700

    fix shelf form

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,4 +1,14 @@
 class ShelvesController < ApplicationController
   def new
+  	@candyShop = CandyShop.find(params[:candy_shop_id])
   end
+  def create
+  	@candyShop = CandyShop.find(params[:candy_shop_id])
+  	@shelf = @candyShop.shelves.create(shelf_params)
+  	redirect_to candy_shop_path(@candyShop)
+  end
+  private
+  	def shelf_params
+  		params.require(:shelf).permit(:name,:capacity,:quantity)
+  	end 
 end
